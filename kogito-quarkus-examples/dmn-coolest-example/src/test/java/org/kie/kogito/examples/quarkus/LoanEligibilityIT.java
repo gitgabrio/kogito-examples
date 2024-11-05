019a966ca93a3047f6d34f539700c3151da5ab71
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2024-11-05 14:08:47 +0100

    [demo_11_15_24_04] Fourth step

--- kogito-quarkus-examples/dmn-coolest-example/src/test/java/org/kie/kogito/examples/quarkus/LoanEligibilityIT.java
+++ kogito-quarkus-examples/dmn-coolest-example/src/test/java/org/kie/kogito/examples/quarkus/LoanEligibilityIT.java
@@ -18,12 +18,12 @@
  */
 package org.kie.kogito.examples.quarkus;
 
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
@@ -34,12 +34,12 @@ public class LoanEligibilityIT {
         // Approved Loan
         given()
                 .body("{" +
-                              "\"Client\": " +
-                              "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
-                              "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
-                              "\"SupremeDirector\" : \"Yes\", " +
-                              "\"Bribe\": 1000" +
-                              "}")
+                        "\"Client\": " +
+                        "{\"age\": 43,\"salary\": 1950,\"existing payments\": 100}," +
+                        "\"Loan\": {\"duration\": 15,\"installment\": 180}, " +
+                        "\"SupremeDirector\" : \"Yes\", " +
+                        "\"Bribe\": 1000" +
+                        "}")
                 .contentType(ContentType.JSON)
                 .when()
                 .post("/LoanEligibility")
